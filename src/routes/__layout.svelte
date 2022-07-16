83200ae3b7f65994e949a4f414e868a73d43a0f2
saweima12 <saweima12@gmail.com>
2022-07-16 19:12:13 +0800

    update package version & add vite hmr support

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,19 +2,30 @@
     import type { Load } from '@sveltejs/kit';
     import { siteConfig } from '$lib/store';
     export const load : Load = async ({fetch}) => {
-
         const response = await fetch("/api/config.json");
         const data: Record<string, any> = await response.json();
         siteConfig.set(data); 
-
-        return {
-        }
-
+        return { }
     }; 
+</script>
+
+<script lang="ts">
+    import { invalidate } from '$app/navigation';
+    import { page } from '$app/stores';
+    import { onContentUpdate } from 'markedpage';
+
+    onContentUpdate((payload: Record<string, any>) => {
+        let slug = $page.params.slug;
+        invalidate(`/api/posts.json`);
+        invalidate(`/api/posts/${slug}.json`);
+        console.log(payload);
+    });
+
+    if (import.meta.hot) {
+
+    }
 </script>
 
 <div class="main-wrapper">
     <slot />
-
-    asoidjsaoid
 </div>
