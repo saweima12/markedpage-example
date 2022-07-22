5bf9379ca0dd294a3b77a40d93298e7640aaf404
saweima12 <saweima12@gmail.com>
2022-07-22 13:11:30 +0800

    Added: site.config reload support setting

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { markedpageVitePlugin } from 'markedpage';
 
+import siteConfig from './src/site.config.js';
+
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), markedpageVitePlugin()]
+	plugins: [sveltekit(), markedpageVitePlugin(siteConfig)]
 };
 
 export default config;
