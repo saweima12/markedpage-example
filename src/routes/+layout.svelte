e6d67d4a896b1ed4eff806d8662864e91bc88d30
saweima12 <saweima12@gmail.com>
2022-08-23 22:46:29 +0800

    adjust pacakge & layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,3 @@
-<script lang="ts">
-    import { invalidate } from '$app/navigation';
-    import { page } from '$app/stores';
-    import { onContentUpdate } from 'markedpage';
-
-    onContentUpdate((payload: Record<string, any>) => {
-        let slug = $page.params.slug;
-        console.log("Update client.")
-        // update endpoint data.
-        invalidate(`/api/posts.json`);
-        invalidate(`/api/posts/${slug}.json`);
-    });
-
-</script>
-
 <div class="main-wrapper">
     <slot />
 </div>
